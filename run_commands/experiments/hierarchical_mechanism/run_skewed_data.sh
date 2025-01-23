d491112c925fe864ae2ee15064a6b20f6e2d4fdc
Boninsegna Fabrizio <fabrizio.boninsegna@phd.unipd.it>
2025-01-24 00:53:52 +0100

    ADD: find constant figures

--- run_commands/experiments/hierarchical_mechanism/run_skewed_data.sh
+++
@@ -1,8 +0,0 @@
-# go back two folder
-cd ../../..
-# enter in the folder preprocess_data
-cd Experiments/skewed_data || exit
-# run the python program synthetic_dataset.py
-
-python experiment_DpNaiveNBS.py --N 2500 --eps 0.5
-python experiment_DpNaiveNBS.py --N 2500 --eps 1
