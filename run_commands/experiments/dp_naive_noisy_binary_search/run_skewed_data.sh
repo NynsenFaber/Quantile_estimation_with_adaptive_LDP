0cc054f28e3969ac51151fd16628a2ec63aaf797
Boninsegna Fabrizio <fabrizio.boninsegna@phd.unipd.it>
2025-01-24 00:56:25 +0100

    ADD: find constant figures

--- run_commands/experiments/dp_naive_noisy_binary_search/run_skewed_data.sh
+++ run_commands/experiments/dp_naive_noisy_binary_search/run_skewed_data.sh
@@ -1,7 +1,7 @@
 # go back two folder
 cd ../../..
 # enter in the folder preprocess_data
-cd Experiments/skewed_data || exit
+cd experiments/skewed_data || exit
 # run the python program synthetic_dataset.py
 
 python experiment_DpNaiveNBS.py --N 2500 --eps 0.5
