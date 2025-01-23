63e3c3b031b41196c8e23d27afbec5e91ec311d9
Boninsegna Fabrizio <fabrizio.boninsegna@phd.unipd.it>
2025-01-23 15:15:16 +0100

    MOD: shell

--- run_commands/experiments/dp_bayes_ss/find_constant.sh
+++ run_commands/experiments/dp_bayes_ss/find_constant.sh
@@ -1,7 +1,7 @@
 # go back two folder
 cd ../../..
 # enter in the folder preprocess_data
-cd Experiments/Pareto_income_data || exit
+cd experiments/pareto_income_data || exit
 # run the python program synthetic_dataset.py
 
 python find_constant.py --N 2500 --B_exp 9 --eps 0.5
