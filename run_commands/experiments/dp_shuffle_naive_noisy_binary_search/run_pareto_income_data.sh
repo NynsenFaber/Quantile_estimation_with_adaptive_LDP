5b0ceb003887ab2e341b605122caa7135eb0b344
Boninsegna Fabrizio <fabrizio.boninsegna@phd.unipd.it>
2025-01-28 14:50:54 +0100

    MOD: DPBayess

--- run_commands/experiments/dp_shuffle_naive_noisy_binary_search/run_pareto_income_data.sh
+++ run_commands/experiments/dp_shuffle_naive_noisy_binary_search/run_pareto_income_data.sh
@@ -5,4 +5,4 @@ cd experiments/pareto_income_data || exit
 # run the python program synthetic_dataset.py
 
 # delta 1e-8 and large N
-python experiment_shuffle_DpNaiveNBS.py --N 10000000 --B_exp 8 --delta 1e-8  --num-exp 20
+python experiment_shuffle_DpNaiveNBS.py --N 10000000 --B_exp 8 --delta 1e-8  --num-exp 200
